705bceb8344c8e3c9b551056f2246e836de2fe6e
Daniel Roich <danielroich@gmail.com>
2020-11-14 13:09:17 +0200

    One last .sh test for formal params

--- examples/ex1/method_inharitance_var.sh
+++ examples/ex1/method_inharitance_var.sh
@@ -1,1 +1,2 @@
 java -jar mjavac.jar unmarshal rename method foo 22 foo2 examples/ex1/method_inheritance_vars.java.xml examples/ex1/method_inheritance_vars_renamed_in_B.java.xml
+java -jar mjavac.jar unmarshal rename var somethingA 38 somethingA_new examples/ex1/method_inheritance_vars.java.xml examples/ex1/method_inheritance_vars_renamed_comlex_formal.java.xml
