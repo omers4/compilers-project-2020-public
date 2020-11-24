c9dfc17403bc4d28adcdf2420e9583f7e47541ea
nofarm <nofarm96@gmail.com>
2020-11-24 23:14:23 +0200

    Added accept calls in LLVM Visitor
    needed for correct traverse, copied from PrintVisitor

--- src/ast/LLVMPrintVisitor.java
+++ src/ast/LLVMPrintVisitor.java
@@ -22,11 +22,12 @@ public class LLVMPrintVisitor implements IVisitorWithField<String> {
     }
 
     private void visitBinaryExpr(BinaryExpr e, String infixSymbol) {
+        e.e1().accept(this);
+        e.e2().accept(this);
     }
 
-
-    @Override
-    public void visit(Program program) {
+    //TODO: del examples
+    public void examples(){
         // Examples of usage:
         builder.append(formatter.formatAlloca("%3", LLVMType.Boolean));
         builder.append("\n");
@@ -48,110 +49,156 @@ public class LLVMPrintVisitor implements IVisitorWithField<String> {
     }
 
     @Override
+    public void visit(Program program) {
+        program.mainClass().accept(this);
+        for (ClassDecl classdecl : program.classDecls()) {
+            classdecl.accept(this);
+        }
+    }
+
+    @Override
     public void visit(ClassDecl classDecl) {
+        for (var fieldDecl : classDecl.fields()) {
+            fieldDecl.accept(this);
+        }
+        for (var methodDecl : classDecl.methoddecls()) {
+            methodDecl.accept(this);
+        }
     }
 
     @Override
     public void visit(MainClass mainClass) {
+        mainClass.mainStatement().accept(this);
     }
 
     @Override
     public void visit(MethodDecl methodDecl) {
+
+        methodDecl.returnType().accept(this);
+        for (var formal : methodDecl.formals()) {
+            formal.accept(this);
+        }
+        for (var varDecl : methodDecl.vardecls()) {
+            varDecl.accept(this);
+        }
+        for (var stmt : methodDecl.body()) {
+            stmt.accept(this);
+        }
+        methodDecl.ret().accept(this);
     }
 
     @Override
     public void visit(FormalArg formalArg) {
+        formalArg.type().accept(this);
     }
 
     @Override
     public void visit(VarDecl varDecl) {
+        varDecl.type().accept(this);
     }
 
     @Override
     public void visit(BlockStatement blockStatement) {
+        for (var s : blockStatement.statements()) {
+            s.accept(this);
+        }
     }
 
     @Override
     public void visit(IfStatement ifStatement) {
+        ifStatement.cond().accept(this);
+        ifStatement.thencase().accept(this);
+        ifStatement.elsecase().accept(this);
     }
 
     @Override
     public void visit(WhileStatement whileStatement) {
+        whileStatement.cond().accept(this);
+        whileStatement.body().accept(this);
     }
 
     @Override
     public void visit(SysoutStatement sysoutStatement) {
+        sysoutStatement.arg().accept(this);
     }
 
     @Override
     public void visit(AssignStatement assignStatement) {
+        assignStatement.rv().accept(this);
     }
 
     @Override
     public void visit(AssignArrayStatement assignArrayStatement) {
+        assignArrayStatement.index().accept(this);
+        assignArrayStatement.rv().accept(this);
     }
 
     @Override
     public void visit(AndExpr e) {
-
+        visitBinaryExpr(e, "&&");
     }
 
     @Override
     public void visit(LtExpr e) {
+        visitBinaryExpr(e, "<");;
     }
 
     @Override
     public void visit(AddExpr e) {
+        visitBinaryExpr(e, "+");;
     }
 
     @Override
     public void visit(SubtractExpr e) {
-
+        visitBinaryExpr(e, "-");
     }
 
     @Override
     public void visit(MultExpr e) {
-
+        visitBinaryExpr(e, "*");
     }
 
     @Override
     public void visit(ArrayAccessExpr e) {
+        e.arrayExpr().accept(this);
+        e.indexExpr().accept(this);
     }
 
     @Override
     public void visit(ArrayLengthExpr e) {
+        e.arrayExpr().accept(this);
     }
 
     @Override
     public void visit(MethodCallExpr e) {
+        e.ownerExpr().accept(this);
+        for (Expr arg : e.actuals()) {
+            arg.accept(this);
+        }
     }
 
     @Override
     public void visit(IntegerLiteralExpr e) {
-
     }
 
     @Override
     public void visit(TrueExpr e) {
-
     }
 
     @Override
     public void visit(FalseExpr e) {
-
     }
 
     @Override
     public void visit(IdentifierExpr e) {
-
     }
 
     public void visit(ThisExpr e) {
-
     }
 
     @Override
     public void visit(NewIntArrayExpr e) {
+        e.lengthExpr().accept(this);
     }
 
     @Override
@@ -160,11 +207,11 @@ public class LLVMPrintVisitor implements IVisitorWithField<String> {
 
     @Override
     public void visit(NotExpr e) {
+        e.e().accept(this);
     }
 
     @Override
     public void visit(IntAstType t) {
-
     }
 
     @Override
@@ -176,7 +223,8 @@ public class LLVMPrintVisitor implements IVisitorWithField<String> {
     }
 
     @Override
-    public void visit(RefType t) {}
+    public void visit(RefType t) {
+    }
 
     @Override
     public String getField() {
