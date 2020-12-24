b6b6dc69e023c1063b7941c46ec242aab13cc652
Daniel Roich <danielroich@gmail.com>
2020-12-24 09:23:12 +0200

    Fixing xml files

--- examples/ex3/methodCallSignature/MethodOverloadingDiffType.java
+++ examples/ex3/methodCallSignature/MethodOverloadingDiffType.java
@@ -12,12 +12,6 @@ class Example {
         return x + x;
     }
 
-    public int other(int y) {
-        int x;
-        x = 1;
-        return x - 1;
-    }
-
     public Example classRet(Example m, int k) {
         int x;
         x = 1;
@@ -28,13 +22,7 @@ class Example {
 class B extends Example{
 
     public Example classRet(C m, int k) {
-        int x;
-        B s;
-        Example k;
-        x = 1;
-        s = new B();
-        k = s.classRet(s,x);
-        return s;
+        return m;
     }
 
     public void test() {
@@ -52,11 +40,6 @@ class B extends Example{
         return x + x;
     }
 
-    public int other(int ss) {
-        int ss;
-        ss = 1;
-        return ss - 1;
-    }
 }
 
 class C extends B{
@@ -64,12 +47,6 @@ class C extends B{
     public int run() {
         x = 2;
         return x + x;
-    }
-
-    public int other(int m) {
-        int ss;
-        ss = 1;
-        return ss - 1;
     }
 
     public void C_Test() {
