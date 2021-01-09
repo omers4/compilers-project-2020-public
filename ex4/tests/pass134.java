e66c40ade52e0c43072fe92095a978b08de9b66a
Daniel Roich <danielroich@gmail.com>
2021-01-09 18:15:35 +0200

    Adding omri tester

---
+++ ex4/tests/pass134.java
@@ -0,0 +1,9 @@
+// PA1 lex comment pass
+class Main { public static        void main(
+    String[
+    ] 
+    args ){
+        System.out.println(hello+omri);
+    }
+}
+class id {} // trailing comment terminated by \r 
---
+++ ex4/tests/pass143.java
@@ -0,0 +1,12 @@
+// PA1 parse field decl pass
+class Main{public static        void main(
+    String[
+    ] 
+    args ){
+        System.out.println(hello+omri);
+    }
+}
+class id {
+    Type x;
+}
+
